4bb1923e5dc291f041e8b02082b3c1e06bd14933
aj <aadityajshah@gmail.com>
2025-10-03 14:08:40 -0400

    Added basic instructions

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,35 @@ function renderHomePage(theme?: Theme): Response {
         </div>
 
         <div class="content">
-          <p>Hello! Welcome to my fun project! This page is meant to be a fun way to display errors, and gather data about them. On the timeline is the ability to generate a report and send it to the webmaster of the originating site. To use not200.com, simply redirect your error pages to https:\/\/not200.com/\<status-code\></p>
+          <p>Hello! Welcome to my fun project! <br />This page is meant to be a fun way to display errors, and gather data about them. On the timeline is the ability to generate a report and send it to the webmaster of the originating site. To use not200.com, simply redirect your error pages to https:\/\/not200.com/\<status-code\> <br /> 
+          If you are using Cloudflare, you can use the Snippet Rule below to push the necessary headers to the end user, and turn on the Send a Report feature.  </p>
+          <br />
+          <code>
+export default {
+  async fetch(request) {
+    // Send original request to the origin
+    const response = await fetch(request);
+
+    // If response is not 200 OK or a redirect, send to another origin
+    if (!response.ok && !response.redirected) {
+      const origCode = response.status;
+      // First, clone the original request to construct a new request
+      const newRequest = new Request(request);
+      // Add a header to identify a re-routed request at the new origin
+      newRequest.headers.set("x-user-email", "[WEBMASTER EMAIL]");
+      newRequest.headers.set("source-cf-ray-id", response.headers.get('cf-ray'))
+      // Clone the original URL
+      // Send request to a different origin / hostname
+      const url = "https://not200.com/" + origCode + "?theme=space";
+      // Serve response to the new request from the origin
+      return await fetch(url, newRequest);
+    }
+
+    // If response is 200 OK or a redirect, serve it
+    return response;
+  },
+};
+          </code>
           <div class="heading">HTTP Status Playground</div>
           <div class="sub">Enter a status code below or try one of the quick links.</div>
           <div class="quick-grid">
